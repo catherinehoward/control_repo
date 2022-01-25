c0809d2c362c18e764f10816e8ac2224ada7b785
Catherine Howard <51328026+catherinehoward@users.noreply.github.com>
2022-01-25 12:17:14 +0000

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -6,6 +6,7 @@ class minecraft {
   file {'/opt/minecraft/server.jar':
     ensure => file,
     source => 'https://launcher.mojang.com/v1/objects/125e5adf40c659fd3bce3e66e67a16bb49ecc1b9/server.jar',
+    before => Service[‘minecraft’],
   }
   
   package {'java':
@@ -25,5 +26,6 @@ class minecraft {
   service {'minecraft':
     ensure => running,
     enable => true,
+    require => [Package[‘java’],File[‘/opt/minecraft/eula.txt'’],File[‘/etc/systemd/system/minecraft.service’]],
   }
 }
